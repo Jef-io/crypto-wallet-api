8f3452a92485d702eef35d1323ac32f5ce1e3237
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-03-24 12:29:18 +0100

    [FEAT] database pool

---
+++ src/service/database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host    : 'localhost',
+    user    : 'root',
+    password: '',
+    database: 'rtai_crypto_wallet',
+    connectionLimit: 10,
+    supportBigNumbers: true
+});
+
+exports.request = (sqlRequest, callback) => {
+  pool.getConnection((err, connection) => {
+    if(err) { console.log(err); callback(true); return; }
+    connection.query(sqlRequest, (err, results) => {
+      connection.release();
+      if(err) { console.log(err); callback(true); return; }
+      callback(false, results);
+    });
+  });
+};
