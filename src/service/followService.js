2653d41916b0905c6d8774cbba06ab9df23a5229
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-10 14:23:40 +0200

    [FEAT] delete & add follow crypto routes

--- src/service/followService.js
+++ src/service/followService.js
@@ -4,12 +4,34 @@ const getFollowedCryptos = async (username) => {
     const sqlRequest = `SELECT crypto_id from followed_cryptos WHERE username='${username}';`
     return new Promise((resolve, reject) => {
         database.request(sqlRequest, (err, result) => {
-            if (err) return reject(err);
+            if (err) reject(err);
+            resolve(result);
+        })
+    })
+}
+
+const addFollowedCrypto = async (username, crypto_id) => {
+    const sqlRequest = `INSERT INTO followed_cryptos (username, crypto_id) VALUES ('${username}', '${crypto_id}');`
+    return new Promise((resolve, reject) => {
+        database.request(sqlRequest, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        })
+    })
+}
+
+const deleteFollowedCrpto = async (username, crypto_id) => {
+    const sqlRequest = `DELETE FROM followed_cryptos WHERE username='${username}' AND crypto_id='${crypto_id}';`
+    return new Promise((resolve, reject) => {
+        database.request(sqlRequest, (err, result) => {
+            if (err) reject(err);
             resolve(result);
         })
     })
 }
 
 export {
-    getFollowedCryptos
+    getFollowedCryptos,
+    addFollowedCrypto,
+    deleteFollowedCrpto
 }
