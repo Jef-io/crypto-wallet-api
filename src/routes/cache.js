13129ff96c7a99e9f7db835da651dc56284d8e76
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-17 12:57:46 +0200

    [FEAT] getcurrent wallet & history

--- src/routes/cache.js
+++ src/routes/cache.js
@@ -2,11 +2,31 @@ const express = require('express')
 const router = express.Router();
 
 import {
-    getWalletHistory
+    getWalletHistory,
+    getCurrentWallet
 } from "../service/cacheService"
+
+import {
+    getCoinsList,
+} from "../externalService/coinGeckoService"
+
 
 router
     .route('/:username')
+    .get(async (req, res) => {
+        try {
+            const wallet = await getCurrentWallet(req.params.username)
+            const cryptos = await getCoinsList();
+            const filtered = cryptos.filter(crypto => wallet.find(coin => coin.crypto_id === crypto.id))
+            const result = filtered.map(crypto => ({...crypto, held: wallet.find(coin => coin.crypto_id === crypto.id).ammount }))
+            res.status(200).send(result)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+
+router
+    .route('/history/:username')
     .get(async (req, res) => {
         try {
             const history = await getWalletHistory(req.params.username)
@@ -15,3 +35,5 @@ router
             res.status(400).send(error)
         }
     })
+
+module.exports = router;
