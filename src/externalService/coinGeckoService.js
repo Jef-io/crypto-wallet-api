efbc77f9d177bfec21b6a9c921f49b56c8ad1eb0
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-19 17:06:47 +0200

    [FEAT] get coin history price

--- src/externalService/coinGeckoService.js
+++ src/externalService/coinGeckoService.js
@@ -14,9 +14,19 @@ const getCoinsList = async () => {
 const getCoinHistory = async (id, days) => {
     try {
         if (days > 90) throw  '90 days is the limit.'
-        const result = await getRequest(`coins/${id}/market_chart?vs_currency=eur&days=${days}&interval=daily`);
-        return result;
+        const data = await getRequest(`coins/${id}/market_chart?vs_currency=eur&days=${days}&interval=daily`);
+        let cryptoInfos = []
+        for (const key in data.prices) {
+            cryptoInfos.push({
+                date: new Date(data.prices[key][0]).getUTCDate(),
+                price: data.prices[key][1],
+                // market_cap: data.market_caps[key][1],
+                // total_volume: data.total_volumes[key][1]
+            })
+        }
+        return cryptoInfos;
     } catch (error) {
+        console.log(error);
         throw error;
     }
 }
